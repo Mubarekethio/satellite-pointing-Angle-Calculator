92373031b64048982a00ad845052efc5c5d6c45b
Mubarekethio <mubare.k1449@gmail.com>
2023-09-14 20:36:54 +0300

    pointing Calculator

--- app/src/main/java/org/mukee/calculatorapp/pointingcalc_mukee/MainActivity.kt
+++ app/src/main/java/org/mukee/calculatorapp/pointingcalc_mukee/MainActivity.kt
@@ -1,15 +1,15 @@
 package org.mukee.calculatorapp.pointingcalc_mukee
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
-import org.jetbrains.annotations.NotNull
-import java.nio.file.DirectoryNotEmptyException
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,13 +21,51 @@ class MainActivity : AppCompatActivity() {
 
         val input1 = findViewById<EditText>(R.id.input1)
         val input2 = findViewById<EditText>(R.id.input2)
-        val input3 = findViewById<EditText>(R.id.input3)
+        var input3 = findViewById<EditText>(R.id.input3)
         val output1 = findViewById<TextView>(R.id.output1)
-        val sat_list = findViewById<Spinner>(R.id.sat_spinner)
+        //val sat_list = findViewById<Spinner>(R.id.sat_spinner)
 
 
         val btnclear = findViewById<Button>(R.id.btnclear)
         val btncalculate = findViewById<Button>(R.id.btncalculate)
+
+        // get reference to the string array that we just created
+        val languages = resources.getStringArray(R.array.programming_languages)
+        // create an array adapter and pass the required parameter
+        // in our case pass the context, drop down layout , and array.
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, languages)
+        // get reference to the autocomplete text view
+        val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        // set adapter to the autocomplete tv to the arrayAdapter
+        //println(autocompleteTV.toString())
+        autocompleteTV.setAdapter(arrayAdapter)
+
+
+        //to get selected value add item click listener
+        //to get selected value add item click listener
+        autocompleteTV.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+            var delimiter = "("
+            var dem= "°"
+            var parts = autocompleteTV.text.toString().split(delimiter)
+            var p=parts[1].split(dem)
+            val dc= "West"
+            if (parts[1].contains(dc)){
+                //print("-"+p[0])
+                input3.setText("-${p[0]}")
+            }else {
+                input3.setText(p[0])
+            }
+
+
+
+            //print(pp[0])
+            Toast.makeText(
+                applicationContext,
+                "" + autocompleteTV.text.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
 
         btncalculate.setOnClickListener() {
 
@@ -35,6 +73,11 @@ class MainActivity : AppCompatActivity() {
                 val B5 = input1.text.toString().toDouble()
                 val C5 = input2.text.toString().toDouble()
                 val D5 = input3.text.toString().toDouble()
+
+                //println(autocompleteTV.text.toString())
+
+
+                //println(D5)
                 val Az: Double = Math.toDegrees(
                     Math.atan(
                         0 - (Math.tan(Math.toRadians(D5) - Math.toRadians(B5)) / (Math.sin(
@@ -47,7 +90,11 @@ class MainActivity : AppCompatActivity() {
                 //longsite+latsite+longSat
                 val El: Double = Math.toDegrees(
                     Math.atan(
-                        (Math.cos(Math.toRadians(D5) - Math.toRadians(B5)) * Math.cos(Math.toRadians(C5)) - 0.15127) / (Math.sin(
+                        (Math.cos(Math.toRadians(D5) - Math.toRadians(B5)) * Math.cos(
+                            Math.toRadians(
+                                C5
+                            )
+                        ) - 0.15127) / (Math.sin(
                             Math.acos(
                                 Math.cos(Math.toRadians(D5) - Math.toRadians(B5)) * Math.cos(
                                     Math.toRadians(C5)
@@ -64,12 +111,21 @@ class MainActivity : AppCompatActivity() {
                         with(output1) {
                             text =
                                 "Azimuth :\t" + Az.toString() + "\nNormal Elevation:\t" + El.toString() + "\nInverted Elevation:\t" + El1.toString()
-                        } //Append(getText(Az.toString()), getText(Az.toString())
-                    }else -> println("")
+                        }
+                        //val inputMethodManager =
+                            //getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+                        // on below line hiding our keyboard.
+                        //inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0)
+                        //val mgr = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                        //mgr.hideSoftInputFromWindow(curEditText.getWindowToken(), 0)//Append(getText(Az.toString()), getText(Az.toString())
+                    }
+
+                    else -> println("")
                 }
-            }catch (e:Exception){
+            } catch (e: Exception) {
                 Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                output1.text=""
+                output1.text = ""
             }
 
 
@@ -85,7 +141,6 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
 }
 
 
